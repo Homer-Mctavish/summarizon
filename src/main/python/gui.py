a229926b88036149856df87b8c4118f5e73dddb5
Homer-Mctavish <49701255+Homer-Mctavish@users.noreply.github.com>
2023-09-28 00:35:11 -0400

    updated with nlkt and worker threads
    yeah, this is epic

--- src/main/python/gui.py
+++ src/main/python/gui.py
@@ -4,14 +4,59 @@ from PyQt5 import uic
 from base import context
 
 from logic import openFile, textExtract, summary
+from PyQt5.QtWidgets import (
+    QWidget, QApplication, QProgressBar, QMainWindow,
+    QHBoxLayout, QPushButton
+)
 
+from PyQt5.QtCore import (
+    Qt, QObject, pyqtSignal, pyqtSlot, QRunnable, QThreadPool
+)
+import time
+
+class WorkerSignals(QObject):
+    progress = pyqtSignal(int)
+
+
+class JobRunner(QRunnable):
+    
+    signals = WorkerSignals()
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.is_paused = False
+        self.is_killed = False
+        
+    @pyqtSlot()
+    def run(self):
+        for n in range(100):
+            self.signals.progress.emit(n + 1)
+            time.sleep(0.1)
+            
+            while self.is_paused:
+                time.sleep(0)
+                
+            if self.is_killed:
+                break
+                
+    def pause(self):
+        self.is_paused = True
+        
+    def resume(self):
+        self.is_paused = False
+        
+    def kill(self):
+        self.is_killed = True
 
 #possibly add a class variable to this including the summarization model so main initializes the model creation and training
 class MainWindow(QMainWindow):
     def __init__(self):
         self.summarizeList = None
+        self.runstatus = True
         super().__init__()
         # Loading the .ui file from the resources
+        #self.threadpool = Worker()
         self.ui = uic.loadUi(context.get_resource("summarizon.ui"), self)
 
         # Binding the button to the print_data function defined in logic.py
@@ -19,3 +64,8 @@ class MainWindow(QMainWindow):
         #self.startSummarizing.clicked.connect(print_data())
         self.extractText.clicked.connect(lambda: textExtract(self))
         self.startSummarizing.clicked.connect(lambda: summary(self))
+        self.pauseSummarizing.clicked.connect(lambda: self.runner.pause)
+        self.stopSummarizing.clicked.connect(lambda: self.runner.kill)
+        self.resumeSummarizing.clicked.connect(lambda: self.runner.resume)
+        
+        
--- src/main/python/logic.py
+++ src/main/python/logic.py
@@ -8,6 +8,8 @@ import numpy as np
 from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import pipeline
 from PyPDF2 import PdfReader
+import nltk
+
 
 billsum = load_dataset("billsum", split="ca_test")
 
@@ -26,6 +28,7 @@ def textExtract(window):
     for page_number in range(number_of_pages):   # use xrange in Py2
         page = reader.pages[page_number]
         page_content = page.extract_text()
+        #"summarize: "+page_content or page content for the nlkt version of summarize
         textlist.append("summarize: "+page_content)
         complete+=1
         window.progressBar.setValue(complete)
@@ -35,17 +38,21 @@ def textExtract(window):
 
 
 def summary(window):
+    #always in the function
     text = window.summarizeList
+    
     summarizer = pipeline("summarization", model="stevhliu/my_awesome_billsum_model")
     percent = 0
     for t in text:
         summarizer(t)
 
         tokenizer = AutoTokenizer.from_pretrained("stevhliu/my_awesome_billsum_model")
-        inputs = tokenizer(t, return_tensors="pt").input_ids
+        #inputs = tokenizer.batch_encode_plus(t, max_length=1024, return_tensors="pt", pad_to_max_length=True).input_ids  # Batch size 1
+
+        inputs = tokenizer(t, max_length=1024, return_tensors="pt", pad_to_max_length=True, truncation=True).input_ids
 
         model = AutoModelForSeq2SeqLM.from_pretrained("stevhliu/my_awesome_billsum_model")
-        outputs = model.generate(inputs, max_new_tokens=100, do_sample=False)
+        outputs = model.generate(inputs, max_new_tokens=1024, do_sample=False)
 
         window.textBrowser.append(tokenizer.decode(outputs[0], skip_special_tokens=True))
         percent+=1
@@ -54,3 +61,55 @@ def summary(window):
     window.mlSummarizationStatusBar.setValue(100)
     print('donezo')
 
+
+#FUCKING PAINFUL
+
+    # text = window.summarizeList
+    # nltk.download('punkt')
+    # checkpoint = "google/pegasus-xsum"
+    # tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    # model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+    # sentonce=''.join(text)
+    # sentences = nltk.tokenize.sent_tokenize(sentonce)
+
+    # # initialize
+    # length = 0
+    # chunk = ""
+    # chunks = []
+    # count = -1
+    # percent = 0
+    # for sentence in text:
+    #     sentence = nltk.tokenize.sent_tokenize(sentence)
+    #     count += 1
+    #     combined_length = len(tokenizer.tokenize(sentence)) + length # add the no. of sentence tokens to the length counter
+
+    #     if combined_length  <= tokenizer.max_len_single_sentence: # if it doesn't exceed
+    #         chunk += sentence + " " # add the sentence to the chunk
+    #         length = combined_length # update the length counter
+
+    #         # if it is the last sentence
+    #         if count == len(sentences) - 1:
+    #             chunks.append(chunk) # save the chunk
+            
+    #     else: 
+    #         chunks.append(chunk) # save the chunk
+    #         # reset 
+    #         length = 0 
+    #         chunk = ""
+
+    #         # take care of the overflow sentence
+    #         chunk += sentence + " "
+    #         length = len(tokenizer.tokenize(sentence))
+
+    #     # inputs
+    #     inputs = [tokenizer(chunk, return_tensors="pt") for chunk in chunks]
+
+    #     # print summary
+    #     for input in inputs:
+    #         output = model.generate(**input)
+    #         window.textBrowser.append(tokenizer.decode(*output, skip_special_tokens=True))
+    #     percent+=1
+    #     window.mlSummarizationStatusBar.setValue(percent)
+
+    # window.mlSummarizationStatusBar.setValue(100)
+    # print('donezo')
